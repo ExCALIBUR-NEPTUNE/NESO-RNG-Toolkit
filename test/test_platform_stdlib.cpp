e70583a93c06581c998df156116fc01337023aef
Will Saunders <will.saunders@ukaea.uk>
2026-03-18 10:58:34 +0000

    Extend tests to request zero samples and an odd number of samples.

--- test/test_platform_stdlib.cpp
+++ test/test_platform_stdlib.cpp
@@ -9,90 +9,94 @@ template <typename VALUE_TYPE> inline void wrapper_uniform() {
   sycl::device device{sycl::default_selector_v};
   sycl::queue queue{device};
 
-  const std::uint64_t seed = 1234;
-  const std::size_t N = 1023;
-  const std::size_t num_bytes = N * sizeof(VALUE_TYPE);
+  for (std::size_t N : {0, 1, 2, 3, 127, 301, 10238, 10239}) {
 
-  const VALUE_TYPE a = -2.0;
-  const VALUE_TYPE b = 2.0;
+    const std::uint64_t seed = 1234;
+    const std::size_t num_bytes = N * sizeof(VALUE_TYPE);
 
-  auto to_test_rng = create_rng<VALUE_TYPE>(
-      Distribution::Uniform<VALUE_TYPE>{a, b}, seed, device, 0, "stdlib");
+    const VALUE_TYPE a = -2.0;
+    const VALUE_TYPE b = 2.0;
 
-  // Generate host side values to test against
-  auto rng = std::mt19937_64{seed};
-  auto dist = std::uniform_real_distribution<VALUE_TYPE>(a, b);
-  std::vector<VALUE_TYPE> correct(N);
-  std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
+    auto to_test_rng = create_rng<VALUE_TYPE>(
+        Distribution::Uniform<VALUE_TYPE>{a, b}, seed, device, 0, "stdlib");
 
-  VALUE_TYPE *d_ptr =
-      static_cast<VALUE_TYPE *>(sycl::malloc_device(num_bytes, queue));
-  ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
-  std::vector<VALUE_TYPE> to_test(N);
-  queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
+    // Generate host side values to test against
+    auto rng = std::mt19937_64{seed};
+    auto dist = std::uniform_real_distribution<VALUE_TYPE>(a, b);
+    std::vector<VALUE_TYPE> correct(N);
+    std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
 
-  ASSERT_EQ(to_test, correct);
+    VALUE_TYPE *d_ptr =
+        static_cast<VALUE_TYPE *>(sycl::malloc_device(num_bytes, queue));
+    ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
+    std::vector<VALUE_TYPE> to_test(N);
+    queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
 
-  ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
-  queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
+    ASSERT_EQ(to_test, correct);
 
-  bool one_different = false;
-  for (std::size_t ix = 0; ix < N; ix++) {
-    if (correct.at(ix) != to_test.at(ix)) {
-      one_different = true;
+    ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
+    queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
+
+    bool one_different = N > 0 ? false : true;
+    for (std::size_t ix = 0; ix < N; ix++) {
+      if (correct.at(ix) != to_test.at(ix)) {
+        one_different = true;
+      }
     }
+    ASSERT_TRUE(one_different);
+
+    std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
+    ASSERT_EQ(to_test, correct);
+
+    ASSERT_TRUE(to_test_rng->get_samples(d_ptr, 0) == SUCCESS);
+
+    sycl::free(d_ptr, queue);
   }
-  ASSERT_TRUE(one_different);
-
-  std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
-  ASSERT_EQ(to_test, correct);
-
-  ASSERT_TRUE(to_test_rng->get_samples(d_ptr, 0) == SUCCESS);
-
-  sycl::free(d_ptr, queue);
 }
 
 template <typename VALUE_TYPE> inline void wrapper_normal() {
   sycl::device device{sycl::default_selector_v};
   sycl::queue queue{device};
 
-  const std::uint64_t seed = 1234;
-  const std::size_t N = 1023;
-  const std::size_t num_bytes = N * sizeof(VALUE_TYPE);
+  for (std::size_t N : {0, 1, 2, 3, 127, 301, 10238, 10239}) {
+    const std::uint64_t seed = 1234;
+    const std::size_t num_bytes = N * sizeof(VALUE_TYPE);
 
-  const VALUE_TYPE mean = 3.0;
-  const VALUE_TYPE stddev = 2.0;
+    const VALUE_TYPE mean = 3.0;
+    const VALUE_TYPE stddev = 2.0;
 
-  auto to_test_rng =
-      create_rng<VALUE_TYPE>(Distribution::Normal<VALUE_TYPE>{mean, stddev},
-                             seed, device, 0, "stdlib");
+    auto to_test_rng =
+        create_rng<VALUE_TYPE>(Distribution::Normal<VALUE_TYPE>{mean, stddev},
+                               seed, device, 0, "stdlib");
 
-  // Generate host side values to test against
-  auto rng = std::mt19937_64{seed};
-  auto dist = std::normal_distribution<VALUE_TYPE>(mean, stddev);
-  std::vector<VALUE_TYPE> correct(N);
-  std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
+    // Generate host side values to test against
+    auto rng = std::mt19937_64{seed};
+    auto dist = std::normal_distribution<VALUE_TYPE>(mean, stddev);
+    std::vector<VALUE_TYPE> correct(N);
+    std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
 
-  VALUE_TYPE *d_ptr =
-      static_cast<VALUE_TYPE *>(sycl::malloc_device(num_bytes, queue));
-  ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
-  std::vector<VALUE_TYPE> to_test(N);
-  queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
+    VALUE_TYPE *d_ptr =
+        static_cast<VALUE_TYPE *>(sycl::malloc_device(num_bytes, queue));
+    ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
+    std::vector<VALUE_TYPE> to_test(N);
+    queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
 
-  ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
-  queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
+    ASSERT_TRUE(to_test_rng->get_samples(d_ptr, N) == SUCCESS);
+    queue.memcpy(to_test.data(), d_ptr, num_bytes).wait_and_throw();
 
-  bool one_different = false;
-  for (std::size_t ix = 0; ix < N; ix++) {
-    if (correct.at(ix) != to_test.at(ix)) {
-      one_different = true;
+    bool one_different = N > 0 ? false : true;
+    for (std::size_t ix = 0; ix < N; ix++) {
+      if (correct.at(ix) != to_test.at(ix)) {
+        one_different = true;
+      }
     }
-  }
-  ASSERT_TRUE(one_different);
+    ASSERT_TRUE(one_different);
 
-  std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
-  ASSERT_EQ(to_test, correct);
-  sycl::free(d_ptr, queue);
+    std::generate(correct.begin(), correct.end(), [&]() { return dist(rng); });
+    ASSERT_EQ(to_test, correct);
+
+    sycl::free(d_ptr, queue);
+  }
 }
 
 } // namespace
